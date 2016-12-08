ecfad4623cfb9929cad5f03ea8a35232dc06af5c
PavelGutin <pavelgutin@gmail.com>
2016-12-08 14:32:32 -0500

    Thank you, linq

---
+++ ProjectEuler/Problem006.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    internal class Problem006
+    {
+        internal static double Solve()
+        {
+            var sumOfSquares = Enumerable.Range(1, 100).Select(x => x * x).Sum();
+            var squareOfSums = Math.Pow(Enumerable.Range(1, 100).Select(x => x).Sum(), 2);
+            return squareOfSums - sumOfSquares;
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -14,7 +14,8 @@ namespace ProjectEuler
             //Console.Write(Problem002.Solve());
             //Console.Write(Problem003.Solve(600851475143));
             //Console.Write(Problem004.Solve());
-            Console.Write(Problem005.Solve());
+            //Console.Write(Problem005.Solve());
+            Console.Write(Problem006.Solve());
             Console.ReadLine();
         }
     }
