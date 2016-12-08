23432226d3cd0f895e8534b90047aac2d52d7f31
PavelGutin <pavelgutin@gmail.com>
2016-12-08 14:28:50 -0500

    problem 5

---
+++ ProjectEuler/Problem005.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    internal class Problem005
+    {
+        internal static int Solve()
+        {
+            int n = 1;
+            
+            while(true)
+            {
+                var isValid = true;
+                for (int i = 2; i <= 20; i++)
+                {
+                    if (n%i != 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid)
+                {
+                    return n;
+                }
+                n++;
+            }
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -13,7 +13,8 @@ namespace ProjectEuler
             //Console.Write(Problem001.Solve());
             //Console.Write(Problem002.Solve());
             //Console.Write(Problem003.Solve(600851475143));
-            Console.Write(Problem004.Solve());
+            //Console.Write(Problem004.Solve());
+            Console.Write(Problem005.Solve());
             Console.ReadLine();
         }
     }
